5c417a58700d19a2aa12087ae78cfdfcf53b3143
Rhuan WSL <rhcarlosweb@gmail.com>
2026-03-18 11:49:47 -0300

    fix: guard generate_uuid_v4 against redeclaration

--- save-contato.php
+++ save-contato.php
@@ -76,11 +76,13 @@ if (!empty($errors)) {
 $sites = array_map(fn($u) => trim((string)$u), $sites);
 
 // UUID v4
-function generate_uuid_v4(): string {
-    $data = random_bytes(16);
-    $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
-    $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
-    return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+if (!function_exists('generate_uuid_v4')) {
+    function generate_uuid_v4(): string {
+        $data = random_bytes(16);
+        $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
+        $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
+        return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+    }
 }
 
 $contact = [
